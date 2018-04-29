c7cf61eb5e331548ea219620287f650d9ea60d2c
Musaib <maq.s89@gmail.com>
2018-04-29 13:09:01 +0300

    upload

---
+++ src/ts/app.ts
@@ -0,0 +1,3 @@
+import '../scss/app.scss';
+let message: string = 'Working...';
+console.log(message);
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/ts/app.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+	publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader"
+        }]
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader'
+            /*options: {
+                transpileOnly: true
+            }*/
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    plugins: [new TsconfigPathsPlugin({ configFile: "./src/ts/tsconfig.json"  })]
+  }
+};
